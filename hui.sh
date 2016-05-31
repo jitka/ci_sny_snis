479d368258cda8fe5619f127eb40cf758739f2ac
Jitka Novotná <jitka@ucw.cz>
2016-05-31 18:26:28 +0200

    scenes working

--- hui.sh
+++ hui.sh
@@ -1,69 +1,53 @@
 function generate {
-for i in {1..6}; 
-do echo """music_directory		\"/home/jitka/git/ci_sny_snis/music\"
-db_file			\"/home/jitka/git/ci_sny_snis/logs/player$i/mpd.db\"
-log_file		\"/home/jitka/git/ci_sny_snis/logs/player$i/mpd.log\"
-pid_file		\"/home/jitka/git/ci_sny_snis/logs/player$i/mpd.pid\"
+echo """music_directory		\"/home/jitka/git/ci_sny_snis/music\"
+db_file			\"/home/jitka/git/ci_sny_snis/logs/player$1/mpd.db\"
+log_file		\"/home/jitka/git/ci_sny_snis/logs/player$1/mpd.log\"
+pid_file		\"/home/jitka/git/ci_sny_snis/logs/player$1/mpd.pid\"
 
-port				\"660$i\"
+port				\"660$1\"
 
 audio_output {
 	type		\"shout\"
 	encoding	\"ogg\"			# optional
-	name		\"Player $i Stream\"
+	name		\"Player $1 Stream\"
 	host		\"localhost\"
 	port		\"8000\"
-	mount		\"/player$i\"
+	mount		\"/player$1\"
 	password	\"hackme\"
 	quality		\"10.0\" #higher quailty mean also shorter ping
 	format		\"44100:16:1\"
 }
 
 filesystem_charset		\"UTF-8\" 
-""" > mpd.player$i.conf
-done
+""" > mpd.player$1.conf
 }
 
-function run {
-for i in {1..6}; 
-do
-    mpd mpd.player$i.conf
-    mpc -p 660$i ls | mpc -p 660$i add
-    mpc -p 660$i play
-done
+function run() {
+    mpd mpd.player$1.conf
+    mpc -p 660$1 ls | mpc -p 660$1 add
+    mpc -p 660$1 play
 }
 
-function add {
-for i in {1..6}; 
-do
-    mpc -p 660$i ls | mpc -p 660$i add
-done
+function add() {
+    mpc -p 660$1 ls | mpc -p 660$1 add
 }
 
-function workshop {
-for i in {1..6}; 
-do
-    mpc -p 660$i crop
-    mpc -p 660$i add Workshop.mp3
-    mpc -p 660$i play 2
-done
+function workshop () {
+    mpc -p 660$1 crop
+    mpc -p 660$1 add Workshop.mp3
+    mpc -p 660$1 play 2
 }
 
 function scene () {
 for i in {1..6}; 
 do
     mpc -p 660$i crop
-    mpc -p 660$i add scenes/scene$1-player$i.mp3
+    mpc -p 660$i add story/scene$1-player$i.mp3
+    mpc -p 660$i add scene$1
     mpc -p 660$i add emotions/
     mpc -p 660$i play 2
 done
 }
-
-
-#-p 6601 crop
-#jitka@kren:~/git/ci_sny_snis $ mpc -p 6601 ls Final | mpc -p 6601 add
-#jitka@kren:~/git/ci_sny_snis $ mpc -p 6601 play 2 
-
 
 function stop {
 for i in {1..6}; 
@@ -72,13 +56,16 @@ do
 done
 }
 
-function update {
-for i in {1..6}; 
-do
-    mpc update -p 660$i
-done
+function update() {
+    mpc update -p 660$1
 }
 
+function all() {
+for i in {1..6}; 
+do
+    $1 $i
+done
+}
 
 function ncmpc {
 #printf "--tab -e \"ncmpc -p 660%s\" " {1..6}
@@ -86,33 +73,31 @@ gnome-terminal --tab -e "ncmpc -p 6601" --tab -e "ncmpc -p 6602" --tab -e "ncmpc
 }
 
 case $1 in
-    generate)
+    prepare)
         echo "Generating mpd configs."
-        generate
+        all generate
+        echo "Updating mpd databases."
+        all update
         ;;
 	run)
 		echo "Running mpd and adding playlist."
-        run
+        all run
 		;;
     stop)
         echo "Stopping mpd."
-        stop
+        all stop
         ;;
     ncmpc)
         echo "Running gnome-terminal with ncmpc tabs."
         ncmpc
         ;;
-    update)
-        echo "Updating mpd databases."
-        update
-        ;;
     add)
         echo "Adding all music to playlist."
-        add
+        all add
         ;;
     workshop)
         echo "Workshop."
-        workshop
+        all workshop
         ;;
     scene)
         echo "Scene $2."
@@ -126,6 +111,7 @@ case $1 in
         ncmpc
         update
         all
+        TODO
         "
 		;;
 esac
